2c1b710ae1b71b8dde3b8da65cd4237a4cf8db8c
balazs4 <balazs4@users.noreply.github.com>
2024-09-10 23:12:47 +0200

    mvp

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, watcher_err := fsnotify.NewWatcher()
+	if watcher_err != nil {
+		panic(watcher_err)
+	}
+	defer watcher.Close()
+	for _, arg := range os.Args[1:] {
+		err := watcher.Add(arg)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "wait for changes...\n")
+	event, ok := <-watcher.Events
+	if ok == false {
+		panic(fmt.Errorf("event not ok"))
+	}
+
+	fmt.Fprintf(os.Stderr, "[%s] %s\n", event.Op, event.Name)
+
+	os.Exit(0)
+}
